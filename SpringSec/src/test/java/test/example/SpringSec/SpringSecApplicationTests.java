59401f053756c4fc44d004803b2ec764e0440dee
gulshensettarova <gulshensettarova@gmail.com>
2024-08-30 16:32:26 +0400

    Initial Project

---
+++ SpringSec/src/test/java/test/example/SpringSec/SpringSecApplicationTests.java
@@ -0,0 +1,13 @@
+package test.example.SpringSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
